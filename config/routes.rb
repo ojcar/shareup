8980898a3ee89795dd82525409289ceb685da324
Oscar Rodriguez <vega@blogactivo.com>
2014-03-04 23:09:52 -0800

    add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Shareup::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   devise_scope :user do
-    get '/api/current_user' => 'users/sessions#show_current_user'
+    get '/api/current_user' => 'users/sessions#show_current_user', :as => 'show_current_user'
+    post '/api/check/is_user' => 'users/users#is_user', :as => 'is_user'
   end
-  
+
   get '/dashboard' => 'welcome#dashboard'
   root :to => 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
