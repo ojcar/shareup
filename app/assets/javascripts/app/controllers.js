6b32a6c67e9f85024c56c8f9cf41193e1f0c95ca
Oscar Rodriguez <vega@blogactivo.com>
2014-03-16 23:10:54 -0700

    add directives

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -1,8 +1,22 @@
 angular.module('myApp.controllers', [])
     .controller('HomeController', 
-        function($scope, ArticleService) {
+        function($scope, session, SessionService, ArticleService, Share) {
+            $scope.user = session.user;
+            
             ArticleService.getLatestFeed()
             .then(function(data) {
                 $scope.articles = data;
-            })
+            });
+
+            $scope.newShare = { recipient: '' };
+            
+            $scope.share = function(recipient, article) {
+                var share = new Share({
+                    url: article.link,
+                    from_user: $scope.user.id,
+                    user: recipient
+                });
+                share.$save();
+                $scope.newShare.recipient = '';
+            }
 });
